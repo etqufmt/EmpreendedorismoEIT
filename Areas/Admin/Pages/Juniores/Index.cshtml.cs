d73402165059184087070ef95f2cf60a2ac20542
etqufmt <45600622+etqufmt@users.noreply.github.com>
2021-07-28 18:42:55 -0400

    paginacao

--- Areas/Admin/Pages/Juniores/Index.cshtml.cs
+++ Areas/Admin/Pages/Juniores/Index.cshtml.cs
@@ -8,30 +8,64 @@ using Microsoft.EntityFrameworkCore;
 using EmpreendedorismoEIT.Data;
 using EmpreendedorismoEIT.Models;
 using EmpreendedorismoEIT.ViewModels;
+using Microsoft.Extensions.Configuration;
+using ContosoUniversity.Utils;
 
 namespace EmpreendedorismoEIT.Areas.Admin.Pages.Juniores
 {
     public class IndexModel : PageModel
     {
-        private readonly EmpreendedorismoEIT.Data.ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
+        private readonly IConfiguration _configuration;
 
-        public IndexModel(EmpreendedorismoEIT.Data.ApplicationDbContext context)
+        public IndexModel(ApplicationDbContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
-        public IList<JunioresListaVM> ListaEJ { get;set; }
+        public PaginatedList<JunioresListaVM> ListaEJ { get;set; }
+        public string CurrentFilter { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string search, string filter, int? pageIndex)
         {
             //Faz um inner join em vez de buscar tudo
-            ListaEJ = await _context.DadosJuniores.Select(e => new JunioresListaVM
+            //ListaEJ = await _context.DadosJuniores.Select(e => new JunioresListaVM
+            //{
+            //    ID = e.EmpresaID,
+            //    Logo = e.Empresa.Logo,
+            //    Nome = e.Empresa.Nome,
+            //    Campus = e.Campus
+            //}).AsNoTracking().ToListAsync();
+
+            if (search != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                search = filter;
+            }
+            CurrentFilter = search;
+
+            var empresasIQ = _context.DadosJuniores.Select(e => new JunioresListaVM
             {
                 ID = e.EmpresaID,
                 Logo = e.Empresa.Logo,
                 Nome = e.Empresa.Nome,
                 Campus = e.Campus
-            }).AsNoTracking().ToListAsync();
+            });
+
+            //Filtrar por pesquisa
+            if (!String.IsNullOrEmpty(search))
+            {
+                empresasIQ = empresasIQ.Where(e => e.Nome.Contains(search));
+            }
+
+            //Fazer consulta
+            var pageSize = _configuration.GetValue("PageSize", 4);
+            ListaEJ = await PaginatedList<JunioresListaVM>.CreateAsync(
+                empresasIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
