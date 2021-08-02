d0f83cb46e3f75efa97c7354cdf01e63428ad26a
etqufmt <45600622+etqufmt@users.noreply.github.com>
2021-08-02 13:58:17 -0400

    croppr working

--- wwwroot/js/admin.validate.js
+++ wwwroot/js/admin.validate.js
@@ -1,10 +1,4 @@
-﻿//Exibir nome de arquivo na caixa de seleção (upload)
-//$(document).on('input', '.custom-file-input', function (evt) {
-//    var name = (evt.target.files[0]?.name) ?? ''
-//    $(this).next('.custom-file-label').html(name);
-//})
-
-//Cria classe "number-only" para entradas de texto
+﻿//Cria classe "number-only" para entradas de texto
 $(document).on('input', '.number-only', function (evt) {
     if (event.inputType != "insertFromPaste") {
         evt.target.value = evt.target.value.replace(/([^0-9]+)/g, "")
@@ -22,3 +16,9 @@ $(document).on('paste', '.number-only', function (evt) {
 $(document).on('input', '.input-len-number', function (evt) {
     evt.target.value = evt.target.value.slice(0, evt.target.maxLength)
 })
+
+//Exibir nome de arquivo na caixa de seleção (upload)
+//$(document).on('input', '.custom-file-input', function (evt) {
+//    var name = (evt.target.files[0]?.name) ?? ''
+//    $(this).next('.custom-file-label').html(name)
+//})
---
+++ wwwroot/js/image.croppr.js
@@ -0,0 +1,76 @@
+﻿var _bsModal = $('#modal-upload')
+var _btnSelect = $('#btn-select')
+var _inputLogoId = 'input-logo'
+//Não funciona com o seletor JQuery
+var _imgCanvas = document.getElementById('img-canvas')
+var _inputLogo = document.getElementById(_inputLogoId)
+var _selected, _filename, _croppr
+
+
+$(document).on("change", '#' + _inputLogoId, (evt) => {
+    var file = evt.target.files[0]
+    if (file) {
+        var url = URL.createObjectURL(file)
+        _imgCanvas.src = url
+        _filename = file.name
+        _bsModal.modal('show')
+    }
+})
+
+_bsModal.on('shown.bs.modal', (evt) => {
+    _selected = false
+    if (_imgCanvas.complete && _imgCanvas.naturalHeight !== 0) {
+        _croppr = new Croppr(_imgCanvas, {
+            //Cálculo invertido: altura/largura
+            aspectRatio: 0.8
+        })
+        _btnSelect.removeAttr('disabled')
+    } else {
+        _btnSelect.attr('disabled', true)
+    }
+})
+
+_btnSelect.click((evt) => {
+    const maxWidth = 800
+    const maxHeight = 640
+    const cropRect = _croppr.getValue()
+    const canvas = document.createElement('canvas')
+    const context = canvas.getContext('2d')
+    canvas.width = cropRect.width > maxWidth ? maxWidth : cropRect.width
+    canvas.height = cropRect.height > maxHeight ? maxHeight : cropRect.height
+    context.drawImage(
+        _croppr.imageEl,
+        cropRect.x,
+        cropRect.y,
+        cropRect.width,
+        cropRect.height,
+        0,
+        0,
+        canvas.width,
+        canvas.height,
+    )
+
+    //Testado com Edge 92 e Firefox 89
+    canvas.toBlob((blob) => {
+        var container = new DataTransfer()
+        var file = new File([blob], _filename, {
+            type: "image/jpeg",
+            lastModified: new Date().getTime()
+        })
+        container.items.add(file)
+        _inputLogo.files = container.files
+        console.log(_inputLogo.files)
+        _selected = true
+        _bsModal.modal('hide')
+    }, 'image/jpeg', 0.9)
+})
+
+_bsModal.on('hide.bs.modal', (evt) => {
+    if (!_selected) {
+        _filename = ''
+        _inputLogo.value = null
+    }
+    $('#' + _inputLogoId).next('.custom-file-label').html(_filename)
+    _croppr?.destroy()
+    _croppr = null
+})
