ebe910e6ed42d9a5d229ed1930e164082106324f
etqufmt <45600622+etqufmt@users.noreply.github.com>
2021-08-02 00:59:18 -0400

    croppr initial

--- wwwroot/js/image.cropper.js
+++ wwwroot/js/image.cropper.js
@@ -1,15 +1,15 @@
 ﻿var bs_modal = $('#modal-upload');
-var img_cropper = document.getElementById('img-cropper');
+var img_canvas = document.getElementById('img_canvas');
 var selected = false;
 var filename;
-var cropper;
+var croppr;
 
 
 $(document).on("change", "#input-logo", (evt) => {
     var file = evt.target.files[0]
     if (file) {
         var url = URL.createObjectURL(file);
-        img_cropper.src = url;
+        img_canvas.src = url;
         filename = file.name
         bs_modal.modal('show');
     }
@@ -17,10 +17,8 @@ $(document).on("change", "#input-logo", (evt) => {
 
 bs_modal.on('shown.bs.modal', (evt) => {
     selected = false
-    cropper = new Cropper(img_cropper, {
-        aspectRatio: 1.5,
-        viewMode: 2,
-        wheelZoomRatio: 0.3
+    croppr = new Croppr(img_canvas, {
+        aspectRatio: 0.8
     });
 }).on('hide.bs.modal', (evt) => {
     if (!selected) {
@@ -28,22 +26,33 @@ bs_modal.on('shown.bs.modal', (evt) => {
         document.getElementById('input-logo').value = null
     }
     $('#input-logo-label').html(filename)
-    cropper.destroy()
-    cropper = null
+    croppr.destroy()
+    croppr = null
 });
 
 $("#btn-select").click((evt) => {
-    canvas = cropper.getCroppedCanvas({
-        maxWidth: 1500,
-        maxHeight: 1000,
-    });
+    const cropRect = croppr.getValue();
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+    canvas.width = cropRect.width > 1000 ? 1000 : cropRect.width;
+    canvas.height = cropRect.height > 800 ? 800 : cropRect.height;
+    context.drawImage(
+        croppr.imageEl,
+        cropRect.x,
+        cropRect.y,
+        cropRect.width,
+        cropRect.height,
+        0,
+        0,
+        canvas.width,
+        canvas.height,
+    );
 
     canvas.toBlob((blob) => {
         var input = document.getElementById('input-logo')
         var container = new DataTransfer()
         var file = new File([blob], "logo.jpg", {
             type: "image/jpeg",
-
             lastModified: new Date().getTime()
         })
         container.items.add(file)
@@ -51,5 +60,5 @@ $("#btn-select").click((evt) => {
         console.log(input.files)
         selected = true;
         bs_modal.modal('hide')
-    }, 'image/jpeg');
+    }, 'image/jpeg', 0.9);
 });
