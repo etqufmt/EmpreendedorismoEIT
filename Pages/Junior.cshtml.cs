724a6a9d45b239bf116367a32a05c959f3fd86b4
etqufmt <45600622+etqufmt@users.noreply.github.com>
2021-09-06 17:06:32 -0400

    pagina juniores

--- Pages/Junior.cshtml.cs
+++ Pages/Junior.cshtml.cs
@@ -2,15 +2,39 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmpreendedorismoEIT.Data;
+using EmpreendedorismoEIT.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmpreendedorismoEIT.Pages
 {
     public class JuniorModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _context;
+
+        public JuniorModel(ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        public List<DadosJunior> ListaAraguia { get; set; }
+
+        public List<DadosJunior> ListaCuiaba { get; set; }
+        
+        public List<DadosJunior> ListaSinop { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var listaEmp = await _context.DadosJuniores
+                .Include(d => d.Empresa)
+                .Where(d => d.Empresa.Situacao == Situacao.ATIVA)
+                .OrderBy(d => d.Empresa.Nome).AsNoTracking().ToListAsync();
+
+            ListaAraguia = listaEmp.Where(e => e.Campus == Campus.ARAGUAIA).ToList();
+            ListaCuiaba = listaEmp.Where(e => e.Campus == Campus.CUIABA).ToList();
+            ListaSinop = listaEmp.Where(e => e.Campus == Campus.SINOP).ToList();
         }
     }
 }
