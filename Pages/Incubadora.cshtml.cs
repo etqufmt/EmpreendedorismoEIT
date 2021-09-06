1ac0b99d770504af7b01bc7ba52ba124a099c2d7
etqufmt <45600622+etqufmt@users.noreply.github.com>
2021-09-06 14:56:39 -0400

    pagina incubadora

--- Pages/Incubadora.cshtml.cs
+++ Pages/Incubadora.cshtml.cs
@@ -2,15 +2,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmpreendedorismoEIT.Data;
+using EmpreendedorismoEIT.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmpreendedorismoEIT.Pages
 {
     public class IncubadoraModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _context;
+
+        public IncubadoraModel(ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        public List<DadosIncubada> ListaEmp { get; set; }
+
+        public async Task OnGet()
+        {
+            ListaEmp = await _context.DadosIncubadas
+                .Include(d => d.Empresa)
+                .Where(d => d.Empresa.Situacao == Situacao.ATIVA)
+                .OrderBy(d => d.Empresa.Nome).AsNoTracking().ToListAsync();
         }
     }
 }
